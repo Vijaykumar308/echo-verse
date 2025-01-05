70610ad99a6d90c45753be52f75b7bddd8e783b1
vijay kumar <308vijaykumar@gmail.com>
2025-01-05 15:47:06 +0530

    logout done

--- frontend/src/pages/dashboard/Sidebar.jsx
+++ frontend/src/pages/dashboard/Sidebar.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import { FaHome, FaPlusCircle, FaShareAlt, FaUserCircle, FaCogs } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'sonner';
+import { useDispatch } from 'react-redux';
+import { setAuthUser } from '../../redux/authSlice';
 
 
 function Sidebar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleLogout = async() => {
+    try {
+      const backendBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
+      const res = await axios.get(`${backendBaseUrl}/logout`);
+
+      if(res.data.success) {
+        toast.success(res.data.message);
+        dispatch(setAuthUser(null));
+        navigate('/login');
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+
+  }
   return (
     <aside className="w-[16%] bg-gray-50 border-r flex flex-col justify-between fixed min-h-screen">
     <div>
@@ -38,12 +61,13 @@ function Sidebar() {
         </ul>
       </nav>
     </div>
-    <NavLink to="/login" className="w-full flex text-lg px-6 py-2 rounded-sm items-center space-x-4 hover:text-blue-500 hover:bg-slate-300  cursor-pointer">
-      <button className="flex items-center space-x-3 py-1">
+
+    <div className="w-full flex text-lg px-6 py-2 rounded-sm items-center space-x-4 hover:text-blue-500 hover:bg-slate-300  cursor-pointer">
+      <button onClick={handleLogout} className="flex items-center space-x-3 py-1">
         <span><BiLogOut size={22}/></span>
         <span>Logout</span>
       </button>
-    </NavLink>
+    </div>
   </aside>
   )
 }
