7cfcd66bf5b1b0aa2e3d2a5565bfd21bee6e7dc0
Vijay Kumar <308vijaykumar@gmail.com>
2025-01-31 23:07:49 +0530

    pages layout for psapp

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+// import { IoMenu } from "react-icons/io5";
+import { TiHome } from "react-icons/ti";
+import { FaShare } from "react-icons/fa";
+
+function Header() {
+  return (
+    <>
+        <div className='bg-blue-300 p-3 fixed w-full top-0'>
+            <div className='logo font-medium text-xl'>Logo</div>
+            <div className='hidden'>
+                <ul>
+                    <li><NavLink>Home</NavLink></li>
+                    <li><NavLink>Sidebar</NavLink></li>
+                    <li><NavLink>Some</NavLink></li>
+                </ul>
+            </div>
+        </div>
+
+        <div className='md:hidden w-full fixed bottom-0 bg-slate-500'>
+            <ul className='flex items-center justify-between py-2 px-3'>
+                <li><NavLink><TiHome className='text-2xl'/></NavLink></li>
+                <li><NavLink><TiHome className='text-2xl'/></NavLink></li>
+                <li><NavLink><FaShare className='text-xl'/></NavLink></li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default Header
